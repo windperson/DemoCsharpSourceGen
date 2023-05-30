a994b2c2478deb53dd150d31eaef7d195c525c55
Isak Pao <windperson@gmail.com>
2023-05-30 19:15:53 +0800

    add source code

---
+++ DemoMain/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoMain;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static partial void HelloFrom(string name);
+}
